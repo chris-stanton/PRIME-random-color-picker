7484d3bc7f118e9c0e1637652a4ca93692d5f5f3
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-14 23:57:02 -0600

    updated css and color generater function

--- client.js
+++ client.js
@@ -1,46 +1,38 @@
 
 $(document).ready(function() {
   var colors = ["red", "green", "yellow", "blue"];
-  var currentColor = "";
-
-  generateBlocks();
-  randomColorPicker();
 
 //button click function
   $("#blockContainer").on("click", "#colorBlocks", function() {
     if($(this).data('color') == currentColor) {
       $("#tryAgainMessage").text("Success! Huge High Five!!!");
-//how do you clear "#tryAgainMessage" if alert is used when correct?
-      //alert("Success! Huge High Five!!!");
-      randomColorPicker();
+    //how do you clear "#tryAgainMessage" if alert is used when correct?
+      randomColor();
     } else {
 //Changes the <h3> on index.html
       $("#tryAgainMessage").text("Try Again!");
     }
   });
-
 //picks random color based from randomNumber() that was in the discription of the homework
-  function randomColorPicker() {
-  //When values for randomNumber(1, colors.length) only 2 colors are being picked, why?
+  function randomColor() {
+    //When values for randomNumber(0, colors.length) only 2 colors are being picked, why?
 //sets value to open string in the $(document).ready(function()
-      currentColor = colors[randomNumber(0, colors.length-1)];
-    }
-
-//Generates Blocks to <div>
-  function generateBlocks() {
+    currentColor = colors[randomNumber(0, colors.length-1)];
+      }
+    var currentColor = "";
+//Appends Blocks to <h1>
+  function makeBlocks() {
     for(var i = 0; i < colors.length; i++) {
-  //can't figure out how to append to multiple <div>'s
-  //decides to go with one <div> and append a new <div> to it
-  //not sure if this is right or best?
-//gives blocks an ID for css
+//gives blocks an ID for css and creates the blocks on the DOM
       $("#blockContainer").append('<h1 id="colorBlocks"></h1>');
       var block = $("#blockContainer").children().last();
       (block).css('background-color', colors[i]);
       (block).data('color', colors[i]);
     }
   }
+  makeBlocks();
+  randomColor();
 });
-
 //randomNumber for randomColor
 //function was from the discription of the homework
 //OK to be in global scope?
